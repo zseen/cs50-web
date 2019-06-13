2167c2befa41922ccad0779531150d5e1c9733fb
zseen <43151120+zseen@users.noreply.github.com>
2019-06-13 13:16:42 +0100

    Implement registration

--- Project2/project2/application.py
+++ Project2/project2/application.py
@@ -6,7 +6,7 @@ import os
 import requests
 
 
-from flask import Flask, jsonify, render_template, request, session
+from flask import Flask, jsonify, render_template, request, session, redirect
 
 app = Flask(__name__)
 #app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
@@ -21,36 +21,50 @@ channelNames = []
 def index():
     return render_template("layout.html")
 
+
 @app.route("/register", methods=["GET", "POST"])
 def login():
     session.clear()
-
     if request.method == "GET":
         return render_template("register.html")
 
     username = request.form.get("username")
+    print(username)
 
     if not username:
         return jsonify({"success": False})
 
     session["username"] = username
-    return jsonify({"success": True, "name": username})
+
+    return jsonify({"success": True, "username": username})
 
 
 @app.route("/createChannel", methods=["GET", "POST"])
 def createChannel():
+    if request.method == "GET":
+        return render_template("createChannel.html")
+
     channelName = request.form.get("channelName")
+
+
     if not channelName:
         return jsonify({"success": False})
 
     if channelName not in channelNames:
         channelNames.append(channelName)
 
-    session["channel"] = channelName    
+    session["channel"] = channelName
+    return render_template("layout.html")
 
 @app.route("/showChannelsList", methods=["GET", "POST"])
 def showChannelsList():
     return render_template("channels.html", channels=channelNames)
+
+
+@app.route("/logout", methods=["GET", "POST"])
+def logout():
+    session.clear()
+    return redirect("/")
 
 
 
---
+++ Project2/project2/static/register.js
@@ -0,0 +1,35 @@
+ document.addEventListener('DOMContentLoaded', () => {
+
+      document.querySelector('#form').onsubmit = () => {
+
+          // Initialize new request
+          const request = new XMLHttpRequest();
+          const username = document.querySelector('#username').value;
+          request.open('POST', '/register');
+
+          // Callback function for when request completes
+          request.onload = () => {
+
+              // Extract JSON data from request
+              const registration = JSON.parse(request.responseText);
+
+              // Update the result div
+              if (registration.success) {
+                  const greeting = `Hello ${registration.username}!`
+                  document.querySelector('#result').innerHTML = greeting;
+              }
+              else {
+                  document.querySelector('#result').innerHTML = 'There was an error.';
+              }
+          }
+
+          // Add data to send with request
+          const userData = new FormData();
+          userData.append('username', username);
+
+          // Send request
+          request.send(userData);
+          return false;
+      };
+
+  });
