f49bcff0b0596b850b079ed1a359af0673b71063
zseen <43151120+zseen@users.noreply.github.com>
2019-07-15 01:00:04 +0100

    Handle user logging in/out

--- Project3/project3/orders/admin.py
+++ Project3/project3/orders/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import SicilianPizza, RegularPizza, Subs, DinnerPlatters, Salads, Toppings, Pasta
 
 
-# Register your models here.
 admin.site.register(SicilianPizza)
 admin.site.register(RegularPizza)
 admin.site.register(Subs)
--- Project3/project3/orders/views.py
+++ Project3/project3/orders/views.py
@@ -1,8 +1,47 @@
-from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-# Create your views here.
+
 def index(request):
-    return HttpResponse("Project 3: WHAT")
+    return render(request, "index.html")
 
 
+def register(request):
+    if request.method == "GET":
+        return render(request, "register.html")
+
+    firstname = request.POST["firstname"]
+    lastname = request.POST["lastname"]
+    username = request.POST["username"]
+    email = request.POST["email"]
+    password = request.POST["password"]
+
+    user = User.objects.create_user(firstname, lastname, username, email, password)
+    user.save()
+    if user:
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "register.html", {"message": "Invalid credentials."})
+
+
+def login_view(request):
+    if request.method == "GET":
+        return render(request, "login.html")
+
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user:
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "login.html", {"message": "Invalid credentials."})
+
+
+def logout_view(request):
+    logout(request)
+    return render(request, "index.html")
