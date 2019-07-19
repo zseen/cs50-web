0e7a8ef08d832596644892cedeedf71716a17541
zseen <43151120+zseen@users.noreply.github.com>
2019-07-19 22:18:01 +0100

    Add classes Food, OnePriceFood and TwoPriceFood in models.py

--- Project3/project3/orders/models.py
+++ Project3/project3/orders/models.py
@@ -1,60 +1,49 @@
 from django.db import models
 
 
-class SicilianPizza(models.Model):
-    name = models.CharField(max_length=64)
-    smallPrice = models.DecimalField(max_digits=4, decimal_places=2)
-    largePrice = models.DecimalField(max_digits=4, decimal_places=2)
+class Food(models.Model):
+    name = models.CharField(max_length=64, default='SOME STRING')
 
+
+class OnePriceFood(Food):
+    price = models.DecimalField(max_digits=4, decimal_places=2, null= True, default='SOME STRING')
+
+
+class TwoPriceFood(Food):
+    smallPrice = models.DecimalField(max_digits=4, decimal_places=2, null= True, default='SOME STRING')
+    largePrice = models.DecimalField(max_digits=4, decimal_places=2, null= True, default='SOME STRING')
+
+
+class SicilianPizza(TwoPriceFood):
     def __str__(self):
         return f"{self.name} - {self.smallPrice} - {self.largePrice}"
 
 
-class RegularPizza(models.Model):
-    name = models.CharField(max_length=64)
-    smallPrice = models.DecimalField(max_digits=4, decimal_places=2)
-    largePrice = models.DecimalField(max_digits=4, decimal_places=2)
-
+class RegularPizza(TwoPriceFood):
     def __str__(self):
         return f"{self.name} - {self.smallPrice} - {self.largePrice}"
 
 
-class Topping(models.Model):
-    name = models.CharField(max_length=64)
-
+class Topping(Food):
     def __str__(self):
         return f"{self.name}"
 
 
-class Sub(models.Model):
-    name = models.CharField(max_length=64)
-    smallPrice = models.DecimalField(max_digits=4, decimal_places=2, blank=True)
-    largePrice = models.DecimalField(max_digits=4, decimal_places=2)
-
+class Sub(TwoPriceFood):
     def __str__(self):
         return f"{self.name} - {self.smallPrice} -{self.largePrice}"
 
 
-class Pasta(models.Model):
-    name = models.CharField(max_length=64)
-    price = models.DecimalField(max_digits=4, decimal_places=2)
-
+class Pasta(OnePriceFood):
     def __str__(self):
         return f"{self.name} - {self.price}"
 
 
-class Salad(models.Model):
-    name = models.CharField(max_length=64)
-    price = models.DecimalField(max_digits=4, decimal_places=2)
-
+class Salad(OnePriceFood):
     def __str__(self):
         return f"{self.name} - {self.price}"
 
 
-class DinnerPlatter(models.Model):
-    name = models.CharField(max_length=64)
-    smallPrice = models.DecimalField(max_digits=4, decimal_places=2)
-    largePrice = models.DecimalField(max_digits=4, decimal_places=2)
-
+class DinnerPlatter(TwoPriceFood):
     def __str__(self):
         return f"{self.name} - {self.smallPrice} - {self.largePrice}"
