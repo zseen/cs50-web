cfdc55563e416a2a92cbc1588924d9e6bc169ac2
zseen <43151120+zseen@users.noreply.github.com>
2019-11-30 17:29:00 +0000

    Start adding test saces in test.py

--- Project3/project3/orders/tests.py
+++ Project3/project3/orders/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+
+from .models import Food, OnePriceFood, FoodOrderItem, ToppingOrderItem, Order, Pasta, DinnerPlatter, RegularPizza, TwoPriceFood
+
+
+class ModelsTestCase(TestCase):
+    def setup(self):
+        food = Food.objects.create(name="food")
+        onePriceFood = OnePriceFood.objects.create(name="onePriceFood", price="1.1")
+        twoPriceFood = TwoPriceFood.objects.create(name="twoPriceFood", smallPrice="1.0", largePrice="1.2")
+        pasta = Pasta.objects.create(category="pasta")
+
+    def test_returnFoodName_foodNameReturnedCorrectly(self):
+        foodName = Food.objects.get(name="food")
+        self.assertEqual("food", foodName)
+
+
