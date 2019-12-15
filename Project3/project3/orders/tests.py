a0307b90a9d5a17ec63701924862a545783fbcb3
zseen <43151120+zseen@users.noreply.github.com>
2019-12-15 21:45:52 +0000

    Add a helper method to PizzaOrderHandlerTestCase

--- Project3/project3/orders/tests.py
+++ Project3/project3/orders/tests.py
@@ -8,18 +8,18 @@ pizzaOrderHandler = PizzaOrderHandler.PizzaOrderHandler()
 
 class ModelsTestCase(TestCase):
     def setUp(self):
-        OnePriceFood.objects.create(name="Pasta", price="1.10")
-        TwoPriceFood.objects.create(name="DinnerPlatter", smallPrice="1.00", largePrice="1.20")
+        OnePriceFood.objects.create(name="BakedZiti", price="1.10")
+        TwoPriceFood.objects.create(name="Antipasto", smallPrice="1.00", largePrice="1.20")
         Salad.objects.create(name="GardenSalad", category="Salad")
 
     def test_getOnePriceFoodByName_getItsPrice_priceMatchesSetUp(self):
-        pasta = OnePriceFood.objects.get(name="Pasta")
-        self.assertEqual("1.10", str(pasta.price))
+        bakedZiti = OnePriceFood.objects.get(name="BakedZiti")
+        self.assertEqual("1.10", str(bakedZiti.price))
 
     def test_getTwoPriceFoodByName_getBothPrices_pricesMatchSetUp(self):
-        dinnerPlatter = TwoPriceFood.objects.get(name="DinnerPlatter")
-        self.assertEqual("1.00", str(dinnerPlatter.smallPrice))
-        self.assertEqual("1.20", str(dinnerPlatter.largePrice))
+        antiPasto = TwoPriceFood.objects.get(name="Antipasto")
+        self.assertEqual("1.00", str(antiPasto.smallPrice))
+        self.assertEqual("1.20", str(antiPasto.largePrice))
 
     def test_getSaladByName_getItsCategory_categoryMatchesSetUp(self):
         gardenSalad = Salad.objects.get(name="GardenSalad")
@@ -31,16 +31,10 @@ class PizzaOrderHandlerTestCase(TestCase):
         RegularPizza.objects.create(name="2 toppings", category="RegularPizza", smallPrice="15.20", largePrice="21.95")
         Topping.objects.create(name="Mushrooms", category="Topping")
         user = User.objects.create_user(username="username")
-        order = Order.objects.create(user=user, orderNumber="1")
-        FoodOrderItem.objects.create(order=order, category="RegularPizza", name="3 toppings", isPizza=True,
-                                     price="21.95")
+        Order.objects.create(user=user, orderNumber="1")
 
-    def test_getToppingAllowance_twoToppingsPizza_equalsTwo(self):
-        twoToppingsPizza = RegularPizza.objects.get(name="2 toppings")
-        toppingAllowance = pizzaOrderHandler.getInitialToppingAllowance(twoToppingsPizza.name)
-        self.assertEqual(2, toppingAllowance)
-
-    def test_addToppingToTwoToppingsPizza_topTwice_zeroRemainingToppingAllowance(self):
+    @staticmethod
+    def createPizzaOrderItem():
         pizzaToTop = RegularPizza.objects.get(name="2 toppings")
         user = User.objects.get(username="username")
         order = Order.objects.get(orderNumber="1", user=user)
@@ -48,12 +42,27 @@ class PizzaOrderHandlerTestCase(TestCase):
                                                price="21.95")
 
         pizzaOrderItem = FoodOrderItem.objects.get(order=order, name="2 toppings")
+        return pizzaOrderItem
+
+    def test_getInitialToppingAllowance_twoToppingsPizza_equalsTwo(self):
+        twoToppingsPizza = RegularPizza.objects.get(name="2 toppings")
+        toppingAllowance = pizzaOrderHandler.getInitialToppingAllowance(twoToppingsPizza.name)
+        self.assertEqual(2, toppingAllowance)
+
+    def test_createPizzaOrderItem_callTestCaseCreatePizzaOrderItem_newlyCreatedPizzaIsCurrentPizza(self):
+        pizzaOrderItem = PizzaOrderHandlerTestCase.createPizzaOrderItem()
         self.assertEqual(pizzaOrderItem, pizzaOrderHandler.getCurrentPizza())
 
-        self.assertEqual(True, pizzaOrderHandler.isCurrentPizzaToppable())
+    def test_addToppingToTwoToppingsPizza_topTwice_zeroRemainingToppingAllowance(self):
+        pizzaToTop = PizzaOrderHandlerTestCase.createPizzaOrderItem()
+        self.assertEqual(pizzaToTop, pizzaOrderHandler.getCurrentPizza())
+
+        originalToppingAllowance = pizzaOrderHandler.getInitialToppingAllowance(pizzaToTop.name)
+        self.assertEqual(2, originalToppingAllowance)
 
         toppingToAdd = Topping.objects.get(name="Mushrooms")
         pizzaOrderHandler.addTopping(toppingToAdd.category, toppingToAdd.name)
+
         remainingToppingAllowance = pizzaOrderHandler.getRemainingToppingAllowance()
         self.assertEqual(1, remainingToppingAllowance)
 
